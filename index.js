ea7f8633b6a44ae7a107437d46f5082e699f150f
Curtis Kesner <ck0562217@otc.edu>
2018-10-28 21:15:30 -0500

    added story and chalk color

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+const chalk = require('chalk');
+console.log(chalk.blue("Hi my name is Curtis Kesner."));
+console.log(chalk.blue("This is a story about my dog Monte."));
+console.log(chalk.blue("My dog Monte is 11 years old and he is a black lab mix."));
+console.log(chalk.blue("Monte likes treats and going for long walks through the neighborhood."));
